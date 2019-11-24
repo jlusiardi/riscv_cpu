b5a8078f518605c1dc39192e063c460e9e5ee6ab
Joachim Lusiardi <joachim@lusiardi.de>
2019-11-24 22:38:00 +0100

    looks good for SB / SH / SW

--- main/memory_control_synth.v
+++ main/memory_control_synth.v
@@ -8,6 +8,8 @@ module memory_control_synth(
         input start,
         input[31:0] address,
         input[2:0] mode,
+        input write_enable,
+        input[31:0] write_data,
         output done,
         output[31:0] read_data
     );
@@ -15,30 +17,38 @@ module memory_control_synth(
     reg[2:0] counter;
     reg[2:0] offset;
     reg[31:0] address_reg;
-    /* verilator lint_off UNUSED */
-    /* verilator lint_off UNDRIVEN */
     wire[7:0] read_data_reg;
     reg[31:0] result_data_reg;
-    reg[7:0] write_data_reg;
+    reg[31:0] write_all_data_reg;
     reg first;
-    /* verilator lint_on UNDRIVEN */
-    /* verilator lint_on UNUSED */
     reg active;
     reg done_reg;
+    reg write_enable_reg;
 
     memory_synth mem ( 
         .read_data(read_data_reg),
         .address(address_reg + {28'b0, offset}),
-        .write_data(write_data_reg),
-        .write_enable(1'b0),
+        .write_data(offset==3 ?
+                        write_all_data_reg[31:24] :
+                        offset==2 ?
+                            write_all_data_reg[23:16] :
+                            offset==1 ?
+                                write_all_data_reg[15:8] :
+                                offset==0 ?
+                                    write_all_data_reg[7:0] :
+                                    8'b0
+                    ),
+        .write_enable(write_enable_reg),
         .clk(clk)
     );
 
     always @(posedge clk) begin
         if (start == 1) begin
             address_reg <= address;
+            write_enable_reg <= write_enable;
+            write_all_data_reg <= write_data;
             active <= 1;
-            first <=1;
+            first <= 1;
             if (mode[1:0] == 2'b0) begin
                 counter <= 1;
                 offset <= 0;
@@ -64,6 +74,7 @@ module memory_control_synth(
                 end
                 if (offset == 0 && counter == 1) begin
                     done_reg <= 1;
+                    write_enable_reg <= 0;
                 end;
                 if (counter == 0) begin
                     done_reg <= 0;
--- tests/memory_control_synth_main.cpp
+++ tests/memory_control_synth_main.cpp
@@ -7,7 +7,72 @@
 using namespace std;
 
 #define ROM memory_control_synth__DOT__mem__DOT__rom__DOT__mem
-#define RAM memory_synth__DOT__ram__DOT__mem
+#define RAM memory_control_synth__DOT__mem__DOT__ram__DOT__mem
+
+class TestMemoryControl_ReadSB: public GeneralTest<Vmemory_control_synth> {
+  public:
+    virtual void test() {
+        top->write_enable = 1;
+        top->start=1;
+        top->mode=FUNC3_SB;
+        top->address=0x1000;
+        top->write_data = 0x00000023;
+        clock_cycle();
+        top->start=0;
+        clock_cycle();
+        ASSERT_EQUALS(top->done, 1);
+        ASSERT_EQUALS(top->RAM[0x0], 0x23);
+        clock_cycle();
+        ASSERT_EQUALS(top->done, 0);
+        clock_cycle();
+    }
+};
+
+class TestMemoryControl_ReadSH: public GeneralTest<Vmemory_control_synth> {
+  public:
+    virtual void test() {
+        top->write_enable = 1;
+        top->start=1;
+        top->mode=FUNC3_SH;
+        top->address=0x1000;
+        top->write_data = 0x00004223;
+        clock_cycle();
+        top->start=0;
+        clock_cycle();
+        clock_cycle();
+        ASSERT_EQUALS(top->done, 1);
+        ASSERT_EQUALS(top->RAM[0x0], 0x23);
+        ASSERT_EQUALS(top->RAM[0x1], 0x42);
+        clock_cycle();
+        ASSERT_EQUALS(top->done, 0);
+        clock_cycle();
+    }
+};
+
+class TestMemoryControl_ReadSW: public GeneralTest<Vmemory_control_synth> {
+  public:
+    virtual void test() {
+        top->write_enable = 1;
+        top->start=1;
+        top->mode=FUNC3_SW;
+        top->address=0x1000;
+        top->write_data = 0x87654321;
+        clock_cycle();
+        top->start=0;
+        clock_cycle();
+        clock_cycle();
+        clock_cycle();
+        clock_cycle();
+        ASSERT_EQUALS(top->done, 1);
+        ASSERT_EQUALS(top->RAM[0x0], 0x21);
+        ASSERT_EQUALS(top->RAM[0x1], 0x43);
+        ASSERT_EQUALS(top->RAM[0x2], 0x65);
+        ASSERT_EQUALS(top->RAM[0x3], 0x87);
+        clock_cycle();
+        ASSERT_EQUALS(top->done, 0);
+        clock_cycle();
+    }
+};
 
 class TestMemoryControl_ReadLB: public GeneralTest<Vmemory_control_synth> {
   public:
@@ -15,6 +80,7 @@ class TestMemoryControl_ReadLB: public GeneralTest<Vmemory_control_synth> {
         top->ROM[0x20] = 0x84;
         top->ROM[0x21] = 0x44;
 
+        top->write_enable = 0;
         top->start=1;
         top->mode=FUNC3_LB;
         top->address=0x20;
@@ -47,6 +113,7 @@ class TestMemoryControl_ReadLBU: public GeneralTest<Vmemory_control_synth> {
         top->ROM[0x20] = 0x84;
         top->ROM[0x21] = 0x44;
 
+        top->write_enable = 0;
         top->start=1;
         top->mode=FUNC3_LBU;
         top->address=0x20;
@@ -81,6 +148,7 @@ class TestMemoryControl_ReadLH: public GeneralTest<Vmemory_control_synth> {
         top->ROM[0x22] = 0x42;
         top->ROM[0x23] = 0x84;
 
+        top->write_enable = 0;
         top->start=1;
         top->mode=FUNC3_LH;
         top->address=0x20;
@@ -117,6 +185,7 @@ class TestMemoryControl_ReadLHU: public GeneralTest<Vmemory_control_synth> {
         top->ROM[0x22] = 0x42;
         top->ROM[0x23] = 0x84;
 
+        top->write_enable = 0;
         top->start=1;
         top->mode=FUNC3_LHU;
         top->address=0x20;
@@ -153,6 +222,7 @@ class TestMemoryControl_ReadLW: public GeneralTest<Vmemory_control_synth> {
         top->ROM[0x22] = 0x42;
         top->ROM[0x23] = 0x84;
 
+        top->write_enable = 0;
         top->start=1;
         top->mode=FUNC3_LW;
         top->address=0x20;
@@ -173,6 +243,9 @@ class TestMemoryControl_ReadLW: public GeneralTest<Vmemory_control_synth> {
 
 int main(int argc, char** argv) {
   cout << "---- Main Memory" << endl;
+  (new TestMemoryControl_ReadSB())->run("vcds/memory_control_synth_read_sb.vcd");
+  (new TestMemoryControl_ReadSH())->run("vcds/memory_control_synth_read_sh.vcd");
+  (new TestMemoryControl_ReadSW())->run("vcds/memory_control_synth_read_sw.vcd");
   (new TestMemoryControl_ReadLB())->run("vcds/memory_control_synth_read_lb.vcd");
   (new TestMemoryControl_ReadLBU())->run("vcds/memory_control_synth_read_lbu.vcd");
   (new TestMemoryControl_ReadLH())->run("vcds/memory_control_synth_read_lh.vcd");
